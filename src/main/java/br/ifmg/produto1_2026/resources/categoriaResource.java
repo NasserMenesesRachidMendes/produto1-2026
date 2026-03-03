1fe774ef18a9597b0d4ba9b16295ce8fd176bb0a
Iury Ribeiro <107949764+iurynho@users.noreply.github.com>
2026-03-03 11:10:47 -0300

    Subidas de Projeto Inicial

---
+++ src/main/java/br/ifmg/produto1_2026/resources/categoriaResource.java
@@ -0,0 +1,33 @@
+package br.ifmg.produto1_2026.resources;
+
+import br.ifmg.produto1_2026.entities.Categoria;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+public class categoriaResource {
+    @GetMapping
+    public ResponseEntity<List<Categoria>> categorias(){
+
+        Categoria categoria1 = new Categoria(1L, "notebook");
+        Categoria categoria2 = new Categoria(1L, "celular");
+        Categoria categoria3 = new Categoria(1L, "livros");
+
+
+        List<Categoria> categorias = new ArrayList<Categoria>();
+        categorias.add(categoria1);
+        categorias.add(categoria2);
+        categorias.add(categoria3);
+        return ResponseEntity.ok().body(categorias);
+    };
+
+
+
+
+}
